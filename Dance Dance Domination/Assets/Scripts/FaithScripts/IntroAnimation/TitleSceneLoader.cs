f7949aa8d51e1ebe790b0315f59db3e5f492af64
fhwang15 <fhwang@inside.artcenter.edu>
2025-03-23 03:31:24 +0900

    timeline playing
    - playing with timelines

--- Dance Dance Domination/Assets/Scripts/FaithScripts/IntroAnimation/TitleSceneLoader.cs
+++ Dance Dance Domination/Assets/Scripts/FaithScripts/IntroAnimation/TitleSceneLoader.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TitleSceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
+        SceneManager.LoadScene("TitleScene", LoadSceneMode.Single);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
