4b2da8be7ab8af1653a8533f2cd9527bd0b6ab3a
avillasenor2002 <125916401+avillasenor2002@users.noreply.github.com>
2025-03-23 12:46:30 -0700

    Lights!

--- Dance Dance Domination/Assets/Scripts/AndrewScripts/HazzardDropperEffect.cs
+++ Dance Dance Domination/Assets/Scripts/AndrewScripts/HazzardDropperEffect.cs
@@ -1,18 +1,54 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class HazzardDropperEffect : MonoBehaviour
+public class HazardDropperEffect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Sprite landedSprite;
+    public AudioClip impactSound;
+    public AudioSource audioSource;
+
+    private SpriteRenderer spriteRenderer;
+
+    public void Initialize(Vector3 startPos, Vector3 endPos, float dropDuration, float fadeOutDuration)
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(DropAndFade(startPos, endPos, dropDuration, fadeOutDuration));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DropAndFade(Vector3 startPos, Vector3 endPos, float dropDuration, float fadeOutDuration)
     {
-        
+        float timer = 0f;
+        transform.position = startPos;
+
+        // Drop to target position
+        while (timer < dropDuration)
+        {
+            transform.position = Vector3.Lerp(startPos, endPos, timer / dropDuration);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = endPos;
+
+        // On impact
+        if (landedSprite != null)
+            spriteRenderer.sprite = landedSprite;
+
+        if (audioSource != null && impactSound != null)
+            audioSource.PlayOneShot(impactSound);
+
+        // Fade out
+        Color originalColor = spriteRenderer.color;
+        timer = 0f;
+
+        while (timer < fadeOutDuration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, timer / fadeOutDuration);
+            spriteRenderer.color = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(gameObject);
     }
 }
