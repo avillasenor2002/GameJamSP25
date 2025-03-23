04710bf93f6811b1e66d0558c8a2e33ebd93f190
avillasenor2002 <125916401+avillasenor2002@users.noreply.github.com>
2025-03-23 10:52:28 -0700

    Visual effects

--- Dance Dance Domination/Assets/Scripts/FaithScripts/PlayerTempoContoller.cs
+++ Dance Dance Domination/Assets/Scripts/FaithScripts/PlayerTempoContoller.cs
@@ -5,54 +5,57 @@ using UnityEngine;
 public class PlayerTempoContoller : MonoBehaviour
 {
     public TempoBarManager TempoBarManager;
-
     public TempoZone tempoZone;
     public AudioSource AudioSource;
 
     public float bpm;
     public float beatThreshold;
     public float changePitchInterval;
-
-    //so this will be what you'll take!
-    //player tempo controller
     public float secondsPerBeat;
-    
-    //public PlayerTempoController globalBeat;
-    //globalBeat = GetComponentInParents<PlayerTempoController>();
-    //globalBeat.secondsPerBeat << This will tell the beat;
 
+    public static event System.Action OnGlobalBeat;
 
     private float nextChangeTime;
+    private float nextBeatTime;
 
     public float minBPM = 80f;
     public float maxBPM = 160f;
 
     private TempoBarShake negativeVisFeedback;
     public GameObject negativeVisSprite;
-
     public GameObject gray;
-
-
 
     private void Start()
     {
         tempoZone = GetComponentInChildren<TempoZone>();
-
         bpm = 130f;
-        secondsPerBeat = 60f / bpm;
+
+        UpdateBeatTiming();
+
         nextChangeTime = Time.time + changePitchInterval;
+        nextBeatTime = Time.time + secondsPerBeat;
+
         negativeVisFeedback = GetComponentInParent<TempoBarShake>();
         negativeVisSprite.SetActive(false);
     }
 
     private void Update()
     {
-        if (Time.time >= nextChangeTime)
+        // Beat logic
+        if (Time.time >= nextBeatTime)
         {
-            ChangeTempoAndTarget(); 
-            nextChangeTime += changePitchInterval;
+            nextBeatTime += secondsPerBeat;
+            OnGlobalBeat?.Invoke();
         }
 
+        // Tempo/pitch change logic
+        if (Time.time >= nextChangeTime)
+        {
+            ChangeTempoAndTarget();
+            nextChangeTime = Time.time + changePitchInterval;
+        }
+
+        // Manual tempo bar interaction
         if (Input.GetKeyDown(KeyCode.Space))
         {
             bool isSuccess = CheckHitTiming();
@@ -60,6 +63,25 @@ public class PlayerTempoContoller : MonoBehaviour
         }
     }
 
+    void ChangeTempoAndTarget()
+    {
+        bpm = Mathf.Round(Random.Range(minBPM, maxBPM) / 10) * 10;
+        AudioSource.pitch = bpm / 120f;
+
+        UpdateBeatTiming();
+        nextBeatTime = Time.time + secondsPerBeat;
+
+        // Reposition the tempo zone inside the gray bar
+        BoxCollider2D grayCollider = gray.GetComponent<BoxCollider2D>();
+        Bounds bounds = grayCollider.bounds;
+
+        float grayMinX = bounds.min.x;
+        float grayMaxX = bounds.max.x;
+        float newX = Random.Range(grayMinX, grayMaxX);
+
+        Vector3 newTargetPos = new Vector3(newX, tempoZone.transform.position.y, tempoZone.transform.position.z);
+        StartCoroutine(SmoothMoveTarget(newTargetPos, 0.5f));
+    }
 
     IEnumerator SmoothMoveTarget(Vector3 newTargetPos, float duration)
     {
@@ -76,38 +98,14 @@ public class PlayerTempoContoller : MonoBehaviour
         tempoZone.transform.position = newTargetPos;
     }
 
-    void ChangeTempoAndTarget()
-    {
-        bpm = Mathf.Round(Random.Range(minBPM, maxBPM) / 10) * 10;
-
-        AudioSource.pitch = bpm / 120f;
-        UpdateBeatTiming();
-
-        BoxCollider2D grayCollider = gray.GetComponent<BoxCollider2D>();
-        Bounds bounds = grayCollider.bounds;
-
-        float grayMinX = bounds.min.x;
-        float grayMaxX = bounds.max.x;
-
-        float newX = Random.Range(grayMinX, grayMaxX);
-
-        Vector3 newTargetPos = new Vector3(newX, tempoZone.transform.position.y, tempoZone.transform.position.z);
-        tempoZone.transform.position = newTargetPos;
-
-        StartCoroutine(SmoothMoveTarget(newTargetPos, 0.5f));
-     }
-
-
     bool CheckHitTiming()
     {
-        //see howmuch time passed
         float songTime = AudioSource.time;
         float nearestBeatTime = Mathf.Floor(songTime / secondsPerBeat) * secondsPerBeat;
         float difference = Mathf.Abs(songTime - nearestBeatTime);
 
         if (difference <= beatThreshold)
         {
-            //StartCoroutine(FlashGreen());
             return true;
         }
         else
@@ -118,17 +116,16 @@ public class PlayerTempoContoller : MonoBehaviour
         }
     }
 
-
     IEnumerator FlashGreen()
     {
-        SpriteRenderer sr = negativeVisSprite.GetComponent<SpriteRenderer>(); 
+        SpriteRenderer sr = negativeVisSprite.GetComponent<SpriteRenderer>();
         Color originColor = sr.color;
 
         sr.color = Color.green;
         negativeVisSprite.SetActive(true);
         yield return new WaitForSeconds(0.4f);
         negativeVisSprite.SetActive(false);
-        sr.color = originColor; 
+        sr.color = originColor;
     }
 
     IEnumerator FlashRed()
@@ -140,7 +137,6 @@ public class PlayerTempoContoller : MonoBehaviour
 
     void UpdateBeatTiming()
     {
-        secondsPerBeat = 60f / bpm; 
+        secondsPerBeat = 60f / bpm;
     }
-
 }
